1558db29bb2aadc8f323c2c1fedff45d6d60fbce
김주원 <kjw11077@naver.com>
2020-07-07 20:05:56 +0900

    Feat: update admin user create
    admin user 만들기

--- joystagram/users/models.py
+++ joystagram/users/models.py
@@ -13,8 +13,8 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
+        user = self.model(email=email, password=password, **extra_fields)
+        user.save()
         return user
 
     def create_user(self, email, password=None, **extra_fields):
