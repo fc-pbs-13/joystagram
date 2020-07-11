89407c51941739efdcf538ed32af6d6c4fdb945e
김주원 <kjw11077@naver.com>
2020-07-11 22:07:18 +0900

    Feat: update serializers
    게시글, 댓글 조회시 대표 댓글,대댓글 노출

--- joystagram/posts/serializers.py
+++ joystagram/posts/serializers.py
@@ -13,6 +13,8 @@ class PostSerializer(serializers.ModelSerializer):
     photos = PhotoSerializer(many=True, read_only=True)
     comments_count = serializers.SerializerMethodField()
 
+    # best_comment = serializers.SerializerMethodField()  # TODO 좋아요가 가장 많은 댓글
+
     class Meta:
         model = Post
         fields = ('id', 'content', 'owner', 'photos', 'comments_count')
@@ -34,15 +36,20 @@ class PostSerializer(serializers.ModelSerializer):
     def get_comments_count(self, obj):
         return obj.comments.count()
 
+    def get_best_comment(self, obj):
+        pass
+
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = ProfileSerializer(read_only=True)
     recomments_count = serializers.SerializerMethodField()
 
+    # best_recomment = serializers.SerializerMethodField()  # TODO 좋아요가 가장 많은 대댓글
+
     class Meta:
         model = Comment
-        fields = ('id', 'content', 'post_id', 'owner', 'recomments', 'recomments_count')
-        read_only_fields = ('post_id', 'owner', 'recomments', 'recomments_count')
+        fields = ('id', 'content', 'owner', 'recomments', 'recomments_count')
+        read_only_fields = ('owner', 'recomments', 'recomments_count')
 
     def create(self, validated_data):
         post = Post.objects.get(pk=self.context["view"].kwargs["post_pk"])
@@ -51,6 +58,9 @@ class CommentSerializer(serializers.ModelSerializer):
 
     def get_recomments_count(self, obj):
         return obj.recomments.count()
+
+    def get_best_recomment(self, obj):
+        pass
 
 
 class ReCommentSerializer(serializers.ModelSerializer):
