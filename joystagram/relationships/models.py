b00f456ff14a480dbf856d2f428fc31a973c83b6
김주원 <kjw11077@naver.com>
2020-07-14 21:38:56 +0900

    Feat: add Follow model

--- joystagram/relationships/models.py
+++ joystagram/relationships/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class Follow(TimeStampedModel):
+    from_user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    to_user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['from_user', 'to_user']
