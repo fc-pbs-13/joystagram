abdd5a2fbd9b89943ed99f25ba9400e61cfc280a
김주원 <kjw11077@naver.com>
2020-07-17 11:20:59 +0900

    Feat: update PostListSerializer
    add like_id field

--- joystagram/posts/views.py
+++ joystagram/posts/views.py
@@ -4,8 +4,9 @@ from rest_framework.viewsets import GenericViewSet
 
 from core.paginations import IDPagination
 from core.permissions import IsOwnerOrReadOnly
+from likes.models import PostLike
 from posts.models import Post
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, PostListSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -20,6 +21,11 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        return super().get_serializer_class()
+
     def filter_queryset(self, queryset):
         if self.action == 'list':
             queryset = queryset.filter()
@@ -27,3 +33,13 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def paginate_queryset(self, queryset):
+        # 모든 포스트
+        page = super().paginate_queryset(queryset)
+        # 해당 포스트의 좋아요중 내가 한것만
+        self.like_id_dict = {}
+        if self.request.user.is_authenticated:
+            like_list = list(PostLike.objects.filter(owner=self.request.user, post__in=page))
+            self.like_id_dict = {like.post_id: like.id for like in like_list}
+        return page
